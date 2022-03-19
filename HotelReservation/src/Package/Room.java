44f26acf4235a958ab6770be3faf98a8faeedc05
jinruiiii <70333940+jinruiiii@users.noreply.github.com>
2022-03-20 01:35:51 +0800

    Added getters and also enum room type

--- HotelReservation/src/Package/Room.java
+++ HotelReservation/src/Package/Room.java
@@ -2,36 +2,61 @@ package Package;
 
 public abstract class Room {
 
-	 public enum BedType{
-	  SINGLE,DOUBLE,MASTER;
-	 }
-	 
-	 public enum Status{
-	  VACANT,OCCUPIED,RESERVED,MAINTENANCE;
-	 }
-	 
-	 protected BedType bedtype;
-	 protected Status status;
-	 protected double weekendRate;
-	 protected double weekdayRate;
-	 private int roomNum;
-	 private Boolean wifi;
-	 private Boolean smoking;
-	 private Boolean view;
-	 
+    public enum BedType{
+        SINGLE,DOUBLE,MASTER;
+    }
 
-	 public Room(int roomNum, Boolean wifi, Boolean smoking, Boolean view) {
-	  
-	  this.roomNum = roomNum;
-	  this.wifi = wifi;
-	  this.smoking = smoking;
-	  this.view = view;
-	  this.status = Status.VACANT;
-	  
-	 }
-	 
-	 public Status checkAvail() {
-	  return this.status;
-	 }
-	 
-	}
+    public enum RoomType{
+        SINGLE, DOUBLE, DELUXE, VIPSUITE;
+    }
+
+    public enum RoomStatus{
+        VACANT,OCCUPIED,RESERVED,MAINTENANCE;
+    }
+
+    protected BedType bedtype;
+    protected RoomType roomtype;
+    protected RoomStatus status;
+    protected double weekendRate;
+    protected double weekdayRate;
+    private int roomNum;
+    private Boolean wifi;
+    private Boolean smoking;
+    private Boolean view;
+
+
+    public Room(int roomNum, Boolean wifi, Boolean smoking, Boolean view) {
+
+        this.roomNum = roomNum;
+        this.wifi = wifi;
+        this.smoking = smoking;
+        this.view = view;
+        this.status = RoomStatus.VACANT;
+
+    }
+
+    public RoomStatus checkAvail() {
+        return this.status;
+    }
+
+    public int getRoomNum(){
+        return roomNum;
+    }
+
+    public RoomType getRoomType(){
+        return roomtype;
+    }
+
+    public Boolean getWifi(){
+        return wifi;
+    }
+
+    public Boolean getSmoking(){
+        return smoking;
+    }
+
+    public Boolean getView(){
+        return view;
+    }
+
+}
