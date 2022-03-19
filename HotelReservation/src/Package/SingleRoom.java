ce5013f887391a5e869c8536ad5425573fa83b28
jinruiiii <70333940+jinruiiii@users.noreply.github.com>
2022-03-20 01:36:24 +0800

    Update SingleRoom.java

--- HotelReservation/src/Package/SingleRoom.java
+++ HotelReservation/src/Package/SingleRoom.java
@@ -3,20 +3,14 @@ package Package;
 
 public class SingleRoom extends Room{
 
- public SingleRoom(int roomNum, Boolean wifi, Boolean smoking, Boolean view) {
-  
-  super(roomNum,wifi,smoking,view);
-  
-  this.bedtype = BedType.SINGLE;
-  this.weekendRate = 100.0;
-  this.weekdayRate = 50.0;
-  this.status = Status.OCCUPIED;
-  
-  
- }
- 
- 
- 
+    public SingleRoom(int roomNum, Boolean wifi, Boolean smoking, Boolean view) {
 
- 
+        super(roomNum,wifi,smoking,view);
+        this.bedtype = BedType.SINGLE;
+        this.roomtype = RoomType.SINGLE;
+        this.weekendRate = 100.0;
+        this.weekdayRate = 50.0;
+        this.status = RoomStatus.OCCUPIED;
+
+    }
 }
