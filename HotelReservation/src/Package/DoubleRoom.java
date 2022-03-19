215f1c4efe87f8ae82c1bb695a83e8bfcb6a5964
jinruiiii <70333940+jinruiiii@users.noreply.github.com>
2022-03-20 01:37:06 +0800

    Create DoubleRoom.java

---
+++ HotelReservation/src/Package/DoubleRoom.java
@@ -0,0 +1,17 @@
+package Assignment
+
+public class DoubleRoom extends Room{
+
+
+    public DoubleRoom (int roomNum, Boolean wifi, Boolean smoking, Boolean view) {
+
+        super(roomNum,wifi,smoking,view);
+        this.bedtype = BedType.DOUBLE;
+        this.roomtype = RoomType.DOUBLE;
+        this.weekendRate = 100.0;
+        this.weekdayRate = 50.0;
+        this.status = RoomStatus.OCCUPIED;
+
+
+    }
+}
