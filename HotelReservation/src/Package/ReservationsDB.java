31fe52b3492aaec6d4bcddcd06dd153904efc94b
MatchaJam88 <97824179+MatchaJam88@users.noreply.github.com>
2022-03-22 14:24:39 +0800

    Update ReservationsDB.java

--- HotelReservation/src/Package/ReservationsDB.java
+++ HotelReservation/src/Package/ReservationsDB.java
@@ -1,4 +1,4 @@
-package Package;
+package Assignment;
 
 import java.util.ArrayList;
 
@@ -36,10 +36,29 @@ public class ReservationsDB {
 		return null;
 	}
 	
+	public void DisplayGuest() {
+		
+		ArrayList<Object> retrievDB = this.ReservationDataBase;
+		
+		for(int i=0;i<retrievDB.size();i++) {
+			
+			ArrayList<Object> row = (ArrayList<Object>) retrievDB.get(i);
+			
+			System.out.println("Guest name:  " + ((Reservation)row.get(1)).getGuest().getIc().getName());
+//			
+			System.out.println("Guest country:  " + ((Reservation)row.get(1)).getGuest().getIc().getCountry());
+			
+			System.out.println("Guest gender:  " + ((Reservation)row.get(1)).getGuest().getIc().getGender());
+			
+			System.out.println("Guest nationality:  " + ((Reservation)row.get(1)).getGuest().getIc().getNationality());
+			
+			System.out.println("Guest address:  " + ((Reservation)row.get(1)).getGuest().getIc().getAddress());
+			
+			System.out.println("Guest contact:  " + ((Reservation)row.get(1)).getGuest().getIc().getContact());
+			
+		}
+	}
 	
-	
-	
-	
-	
+		
 	
 }
