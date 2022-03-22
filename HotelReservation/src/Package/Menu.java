98b6ade17c540e23cb9da1b4eed3f8413cf8d159
jinruiiii <70333940+jinruiiii@users.noreply.github.com>
2022-03-22 23:05:09 +0800

    Update Menu.java

--- HotelReservation/src/Package/Menu.java
+++ HotelReservation/src/Package/Menu.java
@@ -1,28 +1,39 @@
 package Assignment;
 
+
 import java.util.ArrayList;
 
 public class Menu{
 
-	private int numFood;
-	private Food food;
-	ArrayList<Food> foods = new ArrayList<Food>();
-	
-	public void printMenuItems() {
-		System.out.println("List of Menu Items");
-		for (int i =0; i<foods.size();i++) {
-			System.out.println(foods.get(i).getName());
-			System.out.println(foods.get(i).getPrepMethod());
-		}
-	}
-	
-	public boolean addFood(Food food) {
-		for (int i =0; i<foods.size();i++) {
-			if (foods.get(i).getName().equals(food.getName()))
-				return false;
-		}
-		foods.add(food);
-		return true;
-	}
-	
+    private int numFood;
+    private Food food;
+    ArrayList<Food> foods = new ArrayList<Food>();
+
+    public void printMenuItems() {
+        String format = "%-20s%-20s%-20s%n";
+        System.out.printf(format, "Name", "Preparation", "Price($)");
+        System.out.printf(format, "================", "==================", "==================");
+        for (int i =0; i<foods.size();i++) {
+            System.out.printf(format, foods.get(i).getName(), foods.get(i).getPrepMethod(), foods.get(i).getPrice());
+        }
+    }
+
+    public boolean addFood(Food food) {
+        for (int i =0; i<foods.size();i++) {
+            if (foods.get(i).getName().equals(food.getName()))
+                return false;
+        }
+        foods.add(food);
+        return true;
+    }
+
+    public boolean removeFood(String name){
+        for (int i = 0; i < foods.size(); i++){
+            if (foods.get(i).getName().equals(name)){
+                foods.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
 }
