2ebfe274b65d4e1e848de82db2ed4af0e66c9b22
jinruiiii <70333940+jinruiiii@users.noreply.github.com>
2022-03-25 00:59:06 +0800

    Update Reservation.java

--- HotelReservation/src/Package/Reservation.java
+++ HotelReservation/src/Package/Reservation.java
@@ -1,19 +1,11 @@
-package Assignment;
+package Assignment
 
-import java.util.Scanner;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.chrono.ChronoLocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
-public class Reservation implements Payment{
-	
-    public enum RoomType{
-        SINGLEROOM,DOUBLEROOM,DELUXE, VIPSUITE;
-    }
-
-    private RoomType roomtype;
+public class Reservation {
     private Room room;
     private Guest guest;
     private String checkInDate;
@@ -21,51 +13,23 @@ public class Reservation implements Payment{
     private int adults;
     private int children;
     private String code;
-    private double payment;
+
 
     Reservation(Guest guest){
+
         this.guest = guest;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Select check-in date in DD-MM-YYYY");
+        System.out.println("Input check in date");
         this.checkInDate = sc.nextLine();
-        System.out.println("Select check-out date in DD-MM-YYYY");
+        System.out.println("Input check out date");
         this.checkOutDate = sc.nextLine();
         System.out.println("Input number of adults");
         this.adults = sc.nextInt();
         System.out.println("Input number of children");
         this.children = sc.nextInt();
-        //BUFFER
         sc.nextLine();
-        System.out.println("Input Room Type:");
-        String roomInput = sc.nextLine().toUpperCase();
-        switch (roomInput) {
-            case "SINGLE ROOM":
-                this.roomtype = RoomType.SINGLEROOM;
-                Room room = new Room();
-                
-                break;
-            case "DOUBLE ROOM":
-                this.roomtype = RoomType.DOUBLEROOM;
-                break;
-            case "DELUXE":
-                this.roomtype = RoomType.DELUXE;
-                break;
-            case "VIP SUITE":
-                this.roomtype = RoomType.VIPSUITE;
-                break;
-            default:
-                System.out.println("Please enter a valid room type!");
-        }
-
-        generateCode();
-
-        // input for room, check for validity
-
     }
 
-    public RoomType getRoomtype(){
-        return roomtype;
-    }
 
     public String getReservationCode() {
         return code;
@@ -95,7 +59,11 @@ public class Reservation implements Payment{
         return children;
     }
 
-    private void generateCode(){
+    public String getCode(){
+        return code;
+    }
+
+    public void generateCode(){
         String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
                 "0123456789" +
                 "abcdefghijklmnopqrs";
@@ -106,10 +74,6 @@ public class Reservation implements Payment{
             sb.append(AlphaNumericString.charAt(index));
         }
         this.code = sb.toString();
-    }
-
-    public void setRoomtype(RoomType roomtype) {
-        this.roomtype = roomtype;
     }
 
     public void setRoom(Room room) {
@@ -139,61 +103,61 @@ public class Reservation implements Payment{
     public void setCode(String code) {
         this.code = code;
     }
-    
+
+
     public double getPayment() {
-    	// counting the number of weekdays and weekends and add the rates accordingly 
-    	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd mm yyyy");
-    	LocalDate parsedCheckInDate = LocalDate.parse(this.checkInDate);
-    	LocalDate parsedCheckOutDate = LocalDate.parse(this.checkOutDate);
-    	
-    	if (this.roomtype.equals(RoomType.SINGLEROOM)){
-    		for (LocalDate date = parsedCheckInDate; date.isBefore(parsedCheckOutDate); date = date.plusDays(1)){
-    		    if (date.getDayOfWeek() == DayOfWeek.MONDAY || date.getDayOfWeek() == DayOfWeek.TUESDAY || date.getDayOfWeek() == DayOfWeek.WEDNESDAY ||
-    		    	date.getDayOfWeek() == DayOfWeek.THURSDAY || date.getDayOfWeek() == DayOfWeek.FRIDAY) {
-    		    	this.payment += room.weekdayRate;
-    		    }
-    		    else if (date.getDayOfWeek() == DayOfWeek.SATURDAY || date.getDayOfWeek() == DayOfWeek.SUNDAY) {
-    		    	this.payment += room.weekendRate;
-    		    }
-    		}
-    	}
-    	
-    	else if (this.roomtype.equals(RoomType.DOUBLEROOM)){
-			for (LocalDate date = parsedCheckInDate; date.isBefore(parsedCheckOutDate); date = date.plusDays(1)){
-    		    if (date.getDayOfWeek() == DayOfWeek.MONDAY || date.getDayOfWeek() == DayOfWeek.TUESDAY || date.getDayOfWeek() == DayOfWeek.WEDNESDAY ||
-    		    	date.getDayOfWeek() == DayOfWeek.THURSDAY || date.getDayOfWeek() == DayOfWeek.FRIDAY) {
-    		    	this.payment += room.weekdayRate;
-    		    }
-    		    else if (date.getDayOfWeek() == DayOfWeek.SATURDAY || date.getDayOfWeek() == DayOfWeek.SUNDAY) {
-    		    	this.payment += room.weekendRate;
-    		    }
-    		}		    		 
-		}
-		
-		else if (this.roomtype.equals(RoomType.DELUXE)){
-			for (LocalDate date = parsedCheckInDate; date.isBefore(parsedCheckOutDate); date = date.plusDays(1)){
-    		    if (date.getDayOfWeek() == DayOfWeek.MONDAY || date.getDayOfWeek() == DayOfWeek.TUESDAY || date.getDayOfWeek() == DayOfWeek.WEDNESDAY ||
-    		    	date.getDayOfWeek() == DayOfWeek.THURSDAY || date.getDayOfWeek() == DayOfWeek.FRIDAY) {
-    		    	this.payment += room.weekdayRate;
-    		    }
-    		    else if (date.getDayOfWeek() == DayOfWeek.SATURDAY || date.getDayOfWeek() == DayOfWeek.SUNDAY) {
-    		    	this.payment += room.weekendRate;
-    		    }
-    		}
-		}
-		
-		else if (this.roomtype.equals(RoomType.DELUXE)){
-			for (LocalDate date = parsedCheckInDate; date.isBefore(parsedCheckOutDate); date = date.plusDays(1)){
-    		    if (date.getDayOfWeek() == DayOfWeek.MONDAY || date.getDayOfWeek() == DayOfWeek.TUESDAY || date.getDayOfWeek() == DayOfWeek.WEDNESDAY ||
-    		    	date.getDayOfWeek() == DayOfWeek.THURSDAY || date.getDayOfWeek() == DayOfWeek.FRIDAY) {
-    		    	this.payment += room.weekdayRate;
-    		    }
-    		    else if (date.getDayOfWeek() == DayOfWeek.SATURDAY || date.getDayOfWeek() == DayOfWeek.SUNDAY) {
-    		    	this.payment += room.weekendRate;
-    		    }
-    		}
-		}
-    	return this.payment;
+        // counting the number of weekdays and weekends and add the rates accordingly
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd mm yyyy");
+        LocalDate parsedCheckInDate = LocalDate.parse(this.checkInDate);
+        LocalDate parsedCheckOutDate = LocalDate.parse(this.checkOutDate);
+        if (room.getRoomType().equals(Room.RoomType.SINGLE)){
+            for (LocalDate date = parsedCheckInDate; date.isBefore(parsedCheckOutDate); date = date.plusDays(1)){
+                if (date.getDayOfWeek() == DayOfWeek.MONDAY || date.getDayOfWeek() == DayOfWeek.TUESDAY || date.getDayOfWeek() == DayOfWeek.WEDNESDAY ||
+                        date.getDayOfWeek() == DayOfWeek.THURSDAY || date.getDayOfWeek() == DayOfWeek.FRIDAY) {
+                    this.payment += room.weekdayRate;
+                }
+                else if (date.getDayOfWeek() == DayOfWeek.SATURDAY || date.getDayOfWeek() == DayOfWeek.SUNDAY) {
+                    this.payment += room.weekendRate;
+                }
+            }
+        }
+
+        else if (room.getRoomType().equals(Room.RoomType.DOUBLE)){
+            for (LocalDate date = parsedCheckInDate; date.isBefore(parsedCheckOutDate); date = date.plusDays(1)){
+                if (date.getDayOfWeek() == DayOfWeek.MONDAY || date.getDayOfWeek() == DayOfWeek.TUESDAY || date.getDayOfWeek() == DayOfWeek.WEDNESDAY ||
+                        date.getDayOfWeek() == DayOfWeek.THURSDAY || date.getDayOfWeek() == DayOfWeek.FRIDAY) {
+                    this.payment += room.weekdayRate;
+                }
+                else if (date.getDayOfWeek() == DayOfWeek.SATURDAY || date.getDayOfWeek() == DayOfWeek.SUNDAY) {
+                    this.payment += room.weekendRate;
+                }
+            }
+        }
+
+        else if (room.getRoomType().equals(Room.RoomType.DELUXE)){
+            for (LocalDate date = parsedCheckInDate; date.isBefore(parsedCheckOutDate); date = date.plusDays(1)){
+                if (date.getDayOfWeek() == DayOfWeek.MONDAY || date.getDayOfWeek() == DayOfWeek.TUESDAY || date.getDayOfWeek() == DayOfWeek.WEDNESDAY ||
+                        date.getDayOfWeek() == DayOfWeek.THURSDAY || date.getDayOfWeek() == DayOfWeek.FRIDAY) {
+                    this.payment += room.weekdayRate;
+                }
+                else if (date.getDayOfWeek() == DayOfWeek.SATURDAY || date.getDayOfWeek() == DayOfWeek.SUNDAY) {
+                    this.payment += room.weekendRate;
+                }
+            }
+        }
+
+        else if (room.getRoomType().equals(Room.RoomType.VIPSUITE)){
+            for (LocalDate date = parsedCheckInDate; date.isBefore(parsedCheckOutDate); date = date.plusDays(1)){
+                if (date.getDayOfWeek() == DayOfWeek.MONDAY || date.getDayOfWeek() == DayOfWeek.TUESDAY || date.getDayOfWeek() == DayOfWeek.WEDNESDAY ||
+                        date.getDayOfWeek() == DayOfWeek.THURSDAY || date.getDayOfWeek() == DayOfWeek.FRIDAY) {
+                    this.payment += room.weekdayRate;
+                }
+                else if (date.getDayOfWeek() == DayOfWeek.SATURDAY || date.getDayOfWeek() == DayOfWeek.SUNDAY) {
+                    this.payment += room.weekendRate;
+                }
+            }
+        }
+        return this.payment;
     }
 
 }
