d35a807b30ce67f9b528f02f15c7b03c1a04a759
Jlorj <75927686+Jlorj@users.noreply.github.com>
2022-03-28 00:19:12 +0800

    Update ReservationsDB.java

--- HotelReservation/src/Package/ReservationsDB.java
+++ HotelReservation/src/Package/ReservationsDB.java
@@ -61,6 +61,17 @@ public class ReservationsDB {
             System.out.printf(format, ((ArrayList<Object>)ReservationDataBase.get(i)).get(0), ((Reservation)((ArrayList<Object>)ReservationDataBase.get(i)).get(1)).getCheckInDate(),  ((Reservation)((ArrayList<Object>)ReservationDataBase.get(i)).get(1)).getCheckOutDate(),  ((Reservation)((ArrayList<Object>)ReservationDataBase.get(i)).get(1)).getAdults(),  ((Reservation)((ArrayList<Object>)ReservationDataBase.get(i)).get(1)).getChildren(),  ((Reservation)((ArrayList<Object>)ReservationDataBase.get(i)).get(1)).getRoom().getRoomType());
         }
     }
+    
+    public void printReservationFromReservationCode(String ReservationCode) {
+    	String format = "%-20s%-20s%-20s%-20s%-20s%-20s%n";
+        System.out.printf(format, "Reservation Code", "Check-In Date", "Check-Out Date", "# of Adults", "# of Children", "Room Type" );
+        System.out.printf(format, "================", "==================", "==================", "==================", "==================",  "==================");
+        for(int i = 0; i<ReservationDataBase.size(); i++) {
+            if(((String)((ArrayList<Object>)ReservationDataBase.get(i)).get(0)).equals(ReservationCode)) {
+            	System.out.printf(format, ((ArrayList<Object>)ReservationDataBase.get(i)).get(0), ((Reservation)((ArrayList<Object>)ReservationDataBase.get(i)).get(1)).getCheckInDate(),  ((Reservation)((ArrayList<Object>)ReservationDataBase.get(i)).get(1)).getCheckOutDate(),  ((Reservation)((ArrayList<Object>)ReservationDataBase.get(i)).get(1)).getAdults(),  ((Reservation)((ArrayList<Object>)ReservationDataBase.get(i)).get(1)).getChildren(),  ((Reservation)((ArrayList<Object>)ReservationDataBase.get(i)).get(1)).getRoom().getRoomType());
+            }
+        }
+    }
 
 
     public void DisplayGuest() {
